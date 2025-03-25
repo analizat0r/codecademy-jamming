ef0970979dbdc212a62f286aefae317af1fc9589
Tomas Liubertas <tomasliubertas@Tomass-MacBook-Pro.local>
2025-03-25 22:48:56 +0200

    added fonts

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -4,7 +4,7 @@ import styles from "./Track.module.css";
 export default function Track({ trackDetails }) {
   return (
     <div className={styles.track}>
-      <h3>Track: {trackDetails.track}</h3>
+      <p className={styles.title}>Track: {trackDetails.track}</p>
       <p>Artist: {trackDetails.artist}</p>
       <p>Album: {trackDetails.album}</p>
     </div>
